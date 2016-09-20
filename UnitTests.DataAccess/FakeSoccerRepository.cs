d8ff8e6bc250381203c510df1426e7339b3d42c8
jasondavey <jasondavey@hotmail.com>
2016-09-19 23:52:37 -0700

    Added test projects.

---
+++ UnitTests.DataAccess/FakeSoccerRepository.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.repositories.soccer;
+using Domain;
+
+namespace UnitTests.DataAccess
+{
+    public class FakeSoccerRepository : ISoccerTeamRepository
+    {
+        public SoccerTeam FetchTeam<T>(long teamId)
+        {
+            return new SoccerTeam()
+            {
+                Name = "TestName",
+                Players =
+                {
+                    new SoccerPlayer {FirstName = "Jason", LastName = "Davey"},
+                    new SoccerPlayer {FirstName = "Tim", LastName = "Tester"}
+                }
+            };
+        }
+
+        public SoccerPlayer SaveSoccerPlayer(SoccerPlayer soccerPlayer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public SoccerPlayer UpdateSoccerPlayer(SoccerPlayer soccerPlayer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public SoccerPlayer AddSoccerPlayer(SoccerPlayer soccerPlayer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests.DataAccess/SoccerRespositoryTest.cs
@@ -0,0 +1,34 @@
+﻿using DataAccess.repositories.soccer;
+using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTests.DataAccess
+{
+    [TestClass]
+    public class SoccerRespositoryTest
+    {
+        [TestMethod]
+        public void Team_OnFetchTeam_IsPopulated()
+        {
+            //Arrange
+            var soccerTeam = new SoccerTeam()
+            {
+                Name = "TestName",
+                Players =
+                {
+                    new SoccerPlayer {FirstName = "Jason", LastName = "Davey"},
+                    new SoccerPlayer {FirstName = "Tim", LastName = "Tester"}
+                }
+            };
+            var mockSoccerRepository = new Mock<ISoccerTeamRepository>();
+            mockSoccerRepository.Setup(r => r.FetchTeam<SoccerTeam>(1)).Returns(soccerTeam);
+
+
+            //Assert
+            Assert.IsNotNull(soccerTeam);
+            Assert.AreEqual(2, soccerTeam.Players.Count);
+            Assert.AreEqual("TestName", soccerTeam.Name);
+        }
+    }
+}
